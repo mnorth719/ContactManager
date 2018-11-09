46e55d7d1baec2ab0d686db6c2f36394447b5f56
Matt North <matt.north93@gmail.com>
2018-11-08 19:16:49 -0800

    Add functionality to delete contacts via search item

--- ContactManager/ContactService.swift
+++ ContactManager/ContactService.swift
@@ -7,3 +7,73 @@
 //
 
 import Foundation
+import Contacts
+
+class ContactService {
+    enum AuthStatus {
+        case unknown
+        case granted
+        case denied
+    }
+    enum ErrorType: Error {
+        case authDenied
+    }
+
+    private var store: CNContactStore = CNContactStore()
+    private(set) var authStatus: AuthStatus = .unknown
+
+    func requestAccess() {
+        store.requestAccess(for: .contacts) {[weak self] (granted, error) in
+            if granted {
+                self?.authStatus = .granted
+            } else {
+                self?.authStatus = .denied
+            }
+        }
+    }
+
+    func deleteContacts(contactName: String) throws {
+        guard authStatus == .granted else { throw ErrorType.authDenied }
+        let predicate = CNContact.predicateForContacts(matchingName: contactName)
+        let toFetch = [CNContactFamilyNameKey, CNContactGivenNameKey]
+
+        do {
+            let contacts = try store.unifiedContacts(matching: predicate, keysToFetch: toFetch as [CNKeyDescriptor])
+            guard !contacts.isEmpty else {
+                print("No contacts found")
+                return
+            }
+
+            print("contact count: \(contacts.count)")
+            let req = CNSaveRequest()
+            for contact in contacts {
+                let mutableContact = contact.mutableCopy() as! CNMutableContact
+                req.delete(mutableContact)
+            }
+
+            do {
+                try store.execute(req)
+                print("Success, You deleted the contacts")                
+            } catch let e {
+                print("Error = \(e)")
+            }
+        } catch let err{
+            print(err)
+        }
+    }
+
+    func getContactCount(matching name: String) throws -> Int {
+        guard authStatus == .granted else { throw ErrorType.authDenied }
+        let predicate = CNContact.predicateForContacts(matchingName: name)
+        let toFetch = [CNContactFamilyNameKey, CNContactGivenNameKey]
+
+        do {
+            let contacts = try store.unifiedContacts(matching: predicate, keysToFetch: toFetch as [CNKeyDescriptor])
+            return contacts.count
+        } catch let err{
+            print(err)
+        }
+
+        return 0
+    }
+ }
--- ContactManager/ViewController.swift
+++ ContactManager/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var searchField: UITextField!
+
+    private let contactService: ContactService = ContactService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchField.delegate = self
+        contactService.requestAccess()
     }
 
+    @IBAction func search(_ sender: Any) {
+        guard let text = searchField.text, !text.isEmpty else {
+            showError("Enter in a valid search term idiot!")
+            return
+        }
 
+        let count = try? contactService.getContactCount(matching: text)
+        guard let unwrappedCount = count else {
+            showError("No contacts found!")
+            return
+        }
+
+        let alert = UIAlertController(title: "yo!", message: "Found \(unwrappedCount) contact(s). Continue?", preferredStyle: .alert)
+        let cont = UIAlertAction(title: "Go", style: .default) { (action) in
+            try? self.contactService.deleteContacts(contactName: text)
+            let alert = UIAlertController(title: "yo!", message: "all done!", preferredStyle: .alert)
+            let close = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alert.addAction(close)
+            self.present(alert, animated: true, completion: nil)
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cont)
+        alert.addAction(cancel)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    private func showError(_ string: String) {
+        let alert = UIAlertController(title: "yo!", message: string, preferredStyle: .alert)
+        let close = UIAlertAction(title: "Sorry...", style: .cancel, handler: nil)
+        alert.addAction(close)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
