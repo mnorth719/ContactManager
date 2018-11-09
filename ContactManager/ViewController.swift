20da0a0aa53b7aa3d55b04224d82da5adef9843e
Matt North <matt.north93@gmail.com>
2018-11-08 18:13:02 -0800

    Initial Commit

---
+++ ContactManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactManager
+//
+//  Created by Burner on 11/8/18.
+//  Copyright © 2018 mnn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
